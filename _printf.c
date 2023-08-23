14409fc9f5a8aa7973f2b674090dffeba8e96a8e
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-23 00:00:17 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * _printf - functio produces output according to a format
+ * @format: string that contains the text to be written
+ * @...: a variable number of arguments
+ * Return: printed chars
+ */
+int _printf(const char *format, ...)
+{
+	va_list mycharacters;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(mycharacters, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				count++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(mycharacters, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(mycharacters, char *);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				count += str_len;
+			}
+		}
+		format++;
+	}
+	va_end(mycharacters);
+	return (count);
+}
